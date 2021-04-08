b53c0e3b981904593484bb886420a9a9ddb649c0
ngvietchinh311 <58432183+ngvietchinh311@users.noreply.github.com>
2021-04-08 16:47:31 +0700

    Delete lab03/pratice directory

--- lab03/pratice/Die.js
+++
@@ -1,9 +0,0 @@
-function Die(sides) {
-    this.numSides = sides; 
-    this.numRolls = 0;
-    this.roll = roll; // define a pointer to a function
-}
-function roll() {
-    this.numRolls++;
-    return Math.floor(Math.random()*this.numSides) + 1;
-}
