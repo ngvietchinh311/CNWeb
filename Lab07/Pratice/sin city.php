1f8a90eae5400335e3e76192cbd1501cfa700c53
ngvietchinh311 <58432183+ngvietchinh311@users.noreply.github.com>
2021-05-20 16:22:39 +0700

    Add files via upload

---
+++ Lab07/Pratice/sin city.php
@@ -0,0 +1,24 @@
+<?php 
+
+   // set name of XML file 
+   $file = "sins.xml"; 
+
+   // load file 
+   $xml = simplexml_load_file($file) or die ("Unable to load XML file!"); 
+
+   // access each <sin> 
+   echo $xml->sin[0] . "\n"; 
+   echo $xml->sin[1] . "\n"; 
+   echo $xml->sin[2] . "\n"; 
+   echo $xml->sin[3] . "\n"; 
+   echo $xml->sin[4] . "\n"; 
+   echo $xml->sin[5] . "\n"; 
+   echo $xml->sin[6] . "\n"; 
+   
+   // iterate over <sin> element collection 
+   foreach ($xml->sin as $sin) { 
+      echo "$sin\n"; 
+   } 
+
+
+?> 
